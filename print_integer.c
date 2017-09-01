f98ee573cb8c45b1183785070d48fa4c57b431c5
Aditya Ramteke <adityaramteke05icr@gmail.com>
2017-09-01 12:39:07 +0530

    Program for declaring and printing value of integer.

---
+++ print_integer.c
@@ -0,0 +1,8 @@
+// C program for declaring and printing interger.
+#include <stdio.h>
+int main(){
+	int testInteger = 5;	// Declare integer and asign some value.
+	printf("Number = %d\n", testInteger);	// Print value of that integer.
+	return 0;
+}
+
