b25e7dce752962e927507a39501ad56258360f52
Aditya Ramteke <adityaramteke05icr@gmail.com>
2017-09-01 12:51:27 +0530

    program for taking input from user and print that number.

---
+++ take_int_print.c
@@ -0,0 +1,9 @@
+// Program for taking value from user and printing on screen.
+#include <stdio.h>
+int main() {
+	int testInteger;	// Declare one integer.
+	printf("Enter an Integer : ");			
+	scanf("%d",&testInteger);		// Ask user to enter some Integer. 
+	printf("Number = %d\n", testInteger);	// Print that Integer.
+	return 0;
+}
